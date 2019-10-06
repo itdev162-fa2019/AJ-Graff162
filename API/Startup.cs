0b3aaeb1d6a4a6d89d31fd51ef3beec86badcc49
BuildTools <unconfigured@null.spigotmc.org>
2019-10-06 18:42:29 -0500

    Completed Activity 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,11 +22,23 @@ namespace API
             Configuration = configuration;
         }
 
+        private readonly string CorsPolicy = "CorsPolicy";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy(CorsPolicy, policyBuilder =>
+                {
+                    policyBuilder
+                        .WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<DataContext>(opt =>
             {
@@ -47,6 +59,7 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(CorsPolicy);
             app.UseMvc();
         }
     }
